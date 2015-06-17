c62f7628cfd7d04d3a81e18bc23ac779a5b6313c
Mattias Eriksson <thematerik@gmail.com>
2015-06-17 14:00:55 -0700

    UPDATED the package to work with Restberry 1.1.0

--- index.js
+++ index.js
@@ -84,8 +84,7 @@ RestberryPassportGitHub.prototype.findOrCreateUser = function(profile, next) {
     };
     var User = this.restberry.auth.getUser();
     User.findOne(query, function(user) {
-        user.set(data);
-        user.save(function(user) {
+        user.update(data, function(user) {
             next(undefined, user);
         });
     }, function() {
@@ -116,8 +115,8 @@ RestberryPassportGitHub.prototype.setupRoutes = function() {
         .addCustomRoute({
             _controller: function() {
                 return function(req, res, next) {
-                    logger.info('SESSION', 'github login', req.user.getId());
-                    req.user.set('timestampLastLogIn', new Date());
+                    logger.info('SESSION', 'github login', req.user.id);
+                    req.user.timestampLastLogIn = new Date();
                     req.user.save(function(user) {
                         res.redirect(self.returnURL);
                         logger.res(res, self.returnURL);
